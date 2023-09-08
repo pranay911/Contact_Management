32596988ce518173c5ff6e5a17e506b36a65ab2e
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-09-08 17:29:29 +0530

    Bcrypt Installation error

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,22 @@
 const asyncHandler = require("express-async-handler");
-
+// const bcrypt= require("b")
+const User = require("../models/userModel");
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register User" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User Already Register");
+  }
+  //Password Hashing
+  const newUser = await User.res.json({ message: "Register User" });
 });
 
 //@desc Login a user
