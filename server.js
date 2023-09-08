7fb7a960d2e15721848c9a517b6be8616b894300
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-09-08 12:08:34 +0530

    Dotenv Setup Done

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 //Entry Point
-console.log("Server");
+const express = require("express");
+//env setup
+const dotenv = require("dotenv");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("App Started on Port:", port);
+});
