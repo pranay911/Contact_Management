328fb8972d91a55d0d9d131808903240f0f87466
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-09-08 12:15:08 +0530

    Api Tested

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const dotenv = require("dotenv");
 
 const app = express();
 
+app.get("/api/contacts", (req, res) => {
+  res.send("Get All Contacts");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("App Started on Port:", port);
