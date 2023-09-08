678fe880c08637b584b3fd7dbfc45e3c0dfc91ee
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-09-08 12:40:51 +0530

    All Required Routes Created and Tested

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,49 @@
 const express = require("express");
 const router = express.Router();
 
+/*
+ GET - api/contacts/
+ Get all Contacts
+
+*/
 router.get("/", (req, res) => {
   //   res.send("Get All Contacts");
   //   res.status(200).send("Get All Contacts");
   res.json({ message: "Get All Contacts" });
+});
+
+/*
+ GET - api/contacts/
+ Get Particular Contact
+
+*/
+router.get("/:id", (req, res) => {
+  res.status(200).json({ message: `Get Contact for ${req.params.id}` });
+});
+
+/*
+ POST - api/contacts/
+Post contact
+*/
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Create Contact" });
+});
+
+/*
+ PUT - api/contacts/id
+ Update Particular Contact
+
+*/
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+});
+
+/*
+ DELETE - api/contacts/id
+Delete Particular Contact
+*/
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
 });
 
 module.exports = router;
